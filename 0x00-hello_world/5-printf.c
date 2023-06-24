ff6fb27e01d54aa37349a0e2b126febe2bfe2ba7
c-jeje <charltonijeje@ymail.com>
2023-06-23 18:21:54 -0700

    5 print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <stderr.h>
 /**
  * Description: main - prints out a string to stdout.
  * Return: 0 id success.
  */
 int main(void)
 {
-char jeje[] = "with proper grammar, but the outcome is a piece of art,";
+char jeje[] = "with proper grammar, but the outcome is a piece of art, ";
 printf("%s", jeje);
 return (0);
 }
