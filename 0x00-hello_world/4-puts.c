4447d9f2b93dfe8e499157bc6bab605e23210fa6
c-jeje <charltonijeje@ymail.com>
2023-06-23 17:39:47 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
+/** this displays                                                                                                                                    
+ * this                                                                                                                                              
+ */
 int main(void)
 {
   char jeje[] = "\"Programming is like building a multilingual puzzle";
-  
-  puts(jeje);
-    
-  return (0);
+
+    puts(jeje);
+
+    return (0);
 }
