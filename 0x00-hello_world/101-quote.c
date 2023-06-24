0670b0026831ad2f8ab0907c5dcf4a5a518b4d5b
c-jeje <charltonijeje@ymail.com>
2023-06-23 19:15:36 -0700

    jeje

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Description: main - prints out a string to stdout.
+ * Return: 0 id success.
+ */
+int main(void)
+{
+char jeje[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+ fprintf(stderr, "%s", jeje);
+return (1);
+}
