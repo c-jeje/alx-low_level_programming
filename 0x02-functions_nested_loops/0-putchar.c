5deaeb0b7dd5dd9425050b38107309d914c77c84
c-jeje <charltonijeje@ymail.com>
2023-06-25 15:04:58 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**function prints word usng putchar()
+ * no standard libraries
+ */
+/*betty style doc for function main goes there */
+int main(void)
+{
+	char zita = "_putchar\n";
+	int i = 0;
+
+	for (int i = 0; i != '\O'; i++)
+	{
+	putchar(zita[i]);
+	}
+	return (0);
+}
+
