f7b94e02ad3f44dee2b08bec83df157087559846
c-jeje <charltonijeje@ymail.com>
2023-06-26 10:22:44 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
+
+void print_alphabet(void)
+{
+        char c = 'a';
+		int i;
+		int t = 1;
+
+        while (t < 11)
+		{	
+		for (i = 1; i < 27; i++)
+		{
+		_putchar(c);
+		c++;
+		}
+		t++;
+		}
+		_putchar('\n');
+}
