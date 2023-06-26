5f4a11c525a61b47d1e9380b2bda32b5e3020d9d
c-jeje <charltonijeje@ymail.com>
2023-06-26 10:38:33 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,12 +7,13 @@
 
 void print_alphabet_x10(void)
 {
-        char c = 'a';
-		int i;
-		int t = 1;
+        char c;
+	int i;
+	int t = 1;
 
         while (t < 11)
-		{	
+		{
+		c = 'a';	
 		for (i = 1; i < 27; i++)
 		{
 		_putchar(c);
