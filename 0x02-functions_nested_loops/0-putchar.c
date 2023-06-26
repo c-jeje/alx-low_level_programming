b4bcb1856c79850d3b61a0bd1465cb4fe86f0624
c-jeje <charltonijeje@ymail.com>
2023-06-26 08:36:16 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,20 @@
 #include "main.h"
 
-/**function prints word usng putchar()
- * no standard libraries
+/**
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
-/*betty style doc for function main goes there */
 int main(void)
 {
-	char c = "_putchar\n";
-	
-	for (int i = 0; i != '\0'; i++)
-	{
-		putchar(c[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
-}
+
