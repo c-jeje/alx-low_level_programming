6d135fa43cff638a5ae6a9e2b3a6ec3a0a1ff7c2
c-jeje <charltonijeje@ymail.com>
2023-06-27 06:32:10 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - returns the length of a string
+ * @str: points to a character
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i++;
+	}
+}
