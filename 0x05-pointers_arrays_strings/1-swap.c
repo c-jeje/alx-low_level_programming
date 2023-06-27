8be156ac866ab85597065247e8e4f0cd94b81cbd
c-jeje <charltonijeje@ymail.com>
2023-06-27 05:40:56 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * 1-swap.c - swaps a and b values
+ * @a: point t value 1
+ * @b: points to value 2
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+	c = *a
+	*a = *b;
+	*b = c;
+}
