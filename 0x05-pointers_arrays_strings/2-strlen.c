c7588c9905a7534fdaa4464c8bc1e4c6a81b2f4b
c-jeje <charltonijeje@ymail.com>
2023-06-27 05:59:48 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * int _strlen - returns the length of a string
+ * @s: points to a character
+ * Return:the string length
+ */
+int _strlen(char *s)
+{
+	int i;
+	
+	i= 0;
+
+	while (s[i] != '\0')
+	{
+		i++; 
+	}
+	return (i);
+}
