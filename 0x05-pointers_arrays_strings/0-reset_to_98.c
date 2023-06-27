9a43e29f4b1e1cb2307ffe1f72dfd3e2475ef185
c-jeje <charltonijeje@ymail.com>
2023-06-27 05:25:21 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
  * reset_to_98 - updates the value of the variable
  * n: the pointer points to to 98
- * &n: pointer to the variable to update
+ * @n: pointer to the variable to update
  */
 void reset_to_98(int *n)
 {
