3b038ed1556a7237318dc8c691a9f1a57cfaf43d
c-jeje <charltonijeje@ymail.com>
2023-06-27 15:13:16 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - returns the length of a string
+ * @str: points to a character
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i = i + 2;
+	}
+	putchar(10);
+}
