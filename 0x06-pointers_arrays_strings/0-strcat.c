e2dd9b2a43989ccc67bab6f64c990ae19aa8d719
c-jeje <charltonijeje@ymail.com>
2023-06-29 15:37:22 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/** _strcat - appends the src string to the dest string
+ * @dest : variable pointed
+ * @src : variable string pointed
+ * RETURN : returns char
+ */
+char *_strcat(char *dest, char *src)
+{
+	int c_dest, int c_src;
+	
+	c_dest = 0;
+	c_src = 0;
+
+	while (dest[c_dest] != '\0')
+		c_dest++;
+
+	while (src[c_src] != '\0')
+	{
+		dest[c_dest] = src[c_src];
+		c_dest++;
+		c_src +=1;
+	}
+	dest[c_dest] = '\0';
+	
+	return (dest);
+}
