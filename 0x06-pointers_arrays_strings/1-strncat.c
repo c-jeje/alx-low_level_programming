5fcb3e5f53bd4ad8f0d3ce175e37474332b6fde5
c-jeje <charltonijeje@ymail.com>
2023-06-29 16:32:03 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/** _strncat - appends the src string to the dest string
+ * @dest: variable pointed
+ * @src: variable string pointed
+ * @n: variable counter
+ * Return: returns char
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int c_dest;
+	int c_src;
+	
+	c_dest = 0;
+	c_src = 0;
+
+	while (dest[c_dest] != '\0')
+		c_dest++;
+
+	while (c_src < n)
+	{
+		dest[c_dest] = src[c_src];
+
+		c_dest++;
+		c_src++;
+	}
+
+	/*dest[c_dest] = '\0';*/
+	
+	return (dest);
+}
