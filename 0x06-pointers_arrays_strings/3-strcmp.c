d439c8f4dec0f38f37a8e588e836fe1b678f1c43
c-jeje <charltonijeje@ymail.com>
2023-06-30 07:09:21 -0700

    .3-strcmp.c.swp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,14 +15,15 @@ int _strcmp(char *s1, char *s2)
 	{
 		s1++;
 		s2++;
-	}
-	if (*s1 == *s2)
-	{
-		result = 0;
-	}
-	else
-	{
-		result = (*s1 - *s2);
+
+		if (*s1 == *s2)
+		{
+			result = 0;
+		}
+		else
+		{
+			result = (*s1 - *s2);
+		}
 	}
 	return (result);
 }
