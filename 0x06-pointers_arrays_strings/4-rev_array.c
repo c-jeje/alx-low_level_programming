535fb973d74931102c2b387ca5a2c80b083143d7
c-jeje <charltonijeje@ymail.com>
2023-06-30 08:32:36 -0700

    rev array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,12 +12,14 @@ void reverse_array(int *a, int n)
 	int k;
 
 	int i = n - 1;
+
 	int j = 0;
+
 	while (j < n / 2)
 	{
 		k = a[i];	/* temporary storage holde*/
 		a[j] = a[i];	/*start value replaced by end value*/
-		a[i--] = k;	/*last value replaced by first value*/
+		a[i] = k;	/*last value replaced by first value*/
 		
 		j++;
 		/*i--;*/
