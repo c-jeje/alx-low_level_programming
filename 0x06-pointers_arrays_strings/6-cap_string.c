c653d6a7f3e42756b149e7dbad2767d7e71f49bd
c-jeje <charltonijeje@ymail.com>
2023-06-30 13:51:44 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,8 +12,12 @@ char *cap_string(char *str)
 	
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (((str[1] >= 'a') || (str[i] >= 'A')) && ((str[i] <= 'z') || (str[i] >= 'Z')))
-			str[i] = str[i] - 32;
+	/*if (str[i] == '\n' || str[i] == ',' || str[i] == ' '*/ 
+		if (((str[i] >= 32) && (str[i] <= 47)) || ((str[i] >= 58) && (str[i] <= 64)) || ((str[i] >= 123) && (str[i] <= 126)) || ((str[i] == 9) || (str[i] == 10)))
+		{
+			if ((str[i+1] >= 97) && (str[i+1] <= 122))
+				str[i+1] = str[i+1] - 32;
+		}
 	}
 	return (str);
 }
