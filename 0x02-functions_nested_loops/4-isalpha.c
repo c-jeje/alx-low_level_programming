3673ec4c93ab2283b44fd14eeedef08f3d3a15a9
c-jeje <charltonijeje@ymail.com>
2023-07-01 15:01:14 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isalpha  - checks for alphabetic character
+ * @c: integer number
+ * Return: returns an integer value
+ *
+ */
+int _islower(int c)
+{
+	return (((c >= 97) && (c <= 122)) || ((c >= 'A') && (c <= 'Z')));
+}
