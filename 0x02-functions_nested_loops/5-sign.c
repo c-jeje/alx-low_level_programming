934375c5ea391588820076243160c8c5cc2dc1af
c-jeje <charltonijeje@ymail.com>
2023-07-01 15:47:04 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,21 +8,20 @@
  */
 int print_sign(int n)
 {
-	int ret;
 	if (n > 0)
 	{
 		_putchar('+');
-		ret = 1;
+		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-		ret = 0;
+		return (0);
 	}
 	else if (n < 0)
 	{
 		_putchar('1');
-		ret = -1;
+		return (-1);
 	}
-	return (ret);
+		return(-1);
 }
