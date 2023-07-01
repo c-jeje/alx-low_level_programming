8c4b862c9add570b6669e5463e86b5069610297c
c-jeje <charltonijeje@ymail.com>
2023-07-01 14:48:33 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * @c: integer number
+ * Return: returns an integer value
+ *
+ */ 
+int _islower(int c)
+{
+	return ((c >= 97) && (c <= 122));
+
+}
