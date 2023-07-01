7ba5bc7a3d0f50128efde784e3ccde65514e0c1c
c-jeje <charltonijeje@ymail.com>
2023-07-01 04:59:16 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @str: the string variable to be encoded
+ *
+ * Return: resulting string
+ */
+char *rot13(char *str)
+{
+	int i = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		while (((str[i] >= 'a') && (str[i] <= 'z')) || ((str[i] >= 'A') && (str[i] <= 'Z')))
+			{
+				if((str[i] <= 'm') || (str[i] <= 'M'))
+					str[i] = str[i] + 13;
+				else
+					str[i] = str[i] - 13;
+				i++;
+			}
+	}
+	return (str);
+}
