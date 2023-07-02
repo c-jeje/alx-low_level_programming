b34a144ca906000b2856aa51d83b6818ce745e4f
c-jeje <charltonijeje@ymail.com>
2023-07-01 18:02:27 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,14 +3,13 @@
 /**
  * add - adds two integers
  *@a: integer 1
- *@b: inger 2 
+ *@b: inger 2
  * Return: returns the sum of a and b
-*/
+ */
 int add(int a, int b)
 {
 	int z;
 
 	z = a + b;
-	
 	return (z);
 }
