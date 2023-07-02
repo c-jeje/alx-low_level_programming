13001b73bdfd70397af38d8c7269bdeb2c42b9d7
c-jeje <charltonijeje@ymail.com>
2023-07-02 11:39:06 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,22 +7,47 @@
  */
 void print_times_table(int n)
 {
-	int i, k;
-	char product;
+	int i, k, product;
 
 	if ((n <= 15) && (n >= 0))
 	{
 		for (i = 0; i <= n; i++)
 		{
-			for (k = 0; k < n; k++)
+			for (k = 0; k <= n; k++)
 			{
 				product = i * k;
-				_putchar(product);
-				_putchar(',' );
-				_putchar(' ');
+				if (k == 0)
+				{
+					_putchar(product + '0');
+				}
+				if (k > 0)
+				{
+					if (product < 10)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(product + '0');
+					}
+					else if (product >= 10 && product <= 99)	/* two digis*/
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar((product / 10) + '0');
+						_putchar((product % 10) + '0');
+					}
+					else if (product >= 100 && product <= 999)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar((product / 100) + '0');
+						_putchar(((product % 100) / 10) + '0');
+						_putchar(((product % 100) % 10) + '0');
+					}
+				}
 			}
-			product = i * k;
-			_putchar(product);
 			_putchar('\n');
 		}
 	}
