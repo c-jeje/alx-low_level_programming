1f16861bcae7181b8af65a95f653e411bb3a4199
c-jeje <charltonijeje@ymail.com>
2023-07-02 12:19:35 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * 101-natural.c - computes and prints the sum of all the multiples of 3 or 5 below 1024
+ *
+ * Return: returns void
+ */
+void main(void)
+{
+	unsigned long int sum3, sum5, sumup;
+
+	int i, j, k;
+	
+	sum3 = 0;
+	sum5 = 0;
+	sumup = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0)
+			sum3 = +i;
+
+		if (i % 5 == 0)
+			sum5 = +i;
+	}
+	sumup = sum3 + sum5;
+
+	_putchar(sumup + '0');
+	_putchar('\n' + '0');
+}
