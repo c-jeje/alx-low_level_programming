d8737bb455c1fb42e37966c8a91a821b0b11b0d0
c-jeje <charltonijeje@ymail.com>
2023-07-06 15:22:33 -0700

    my own code

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,23 +9,21 @@
  **/
 int _pow_recursion(int x, int y)
 {
-        int a = 0;
-        if (y < 0)
-        {
-                return (-1);
-        }
-        if (y == 0)
-        {
-                return (1);
-        }
-        else if (y != 0)
-        {
-            a = x * _pow_recursion(x, y - 1);
-        }
-        return (a);
+	int a = 0;
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	else if (y != 0)
+	{
+		a = x * _pow_recursion(x, y - 1);
+	}
+	return (a);
 }
-
-
 /**
 int main (void)
 {
