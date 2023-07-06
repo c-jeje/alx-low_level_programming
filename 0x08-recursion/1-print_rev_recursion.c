3c8af93ebae7f77d9a6aeb1685c93a69a40b14d0
c-jeje <charltonijeje@ymail.com>
2023-07-05 17:18:35 -0700

    futi

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,9 +6,9 @@
  *
  * Return: returns void
  **/
-void _print_rev_recursion(char *s);
+void _print_rev_recursion(char *s)
 {
-{	if (*s)
+	if (*s)
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
