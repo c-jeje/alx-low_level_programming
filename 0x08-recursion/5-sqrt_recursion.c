968110da66b2e79acaffeb6478de473bfb37ac9b
c-jeje <charltonijeje@ymail.com>
2023-07-06 17:59:06 -0700

    ./5-sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,22 +11,30 @@ int test_sqrt(int n, int i);
  **/
 int _sqrt_recursion(int n)
 {
-    if ( n < 0)
-    {
-        return (-1);
-    }
-    return (test_sqrt(n, 0));
+	if ( n < 0)
+	{
+	return (-1);
+	}
+	return (test_sqrt(n, 0));
 }
 
+/**
+ * test_sqrt - returns the natural square root of a number.
+ * if n does not have a natural square root, the function should return -1
+ * @n: the number to be tested
+ * @i: numbers less than n
+ *
+ * Return: it returns natural square root 
+ **/
 int test_sqrt(int n, int i)
 {
 	if ( i * i > n)
-    {
-        return (-1);
-    }
-    if ((i * i) == n)
-    {
-        return (i);
-    }
+	{
+	return (-1);
+	}
+	if ((i * i) == n)
+	{
+	return (i);
+	}
    return (test_sqrt(n, i + 1));
 }
