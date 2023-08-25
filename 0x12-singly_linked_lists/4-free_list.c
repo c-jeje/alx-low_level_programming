c8a157c13e2e5e9e256e0961a6f90995889831ea
c-jeje <charltonijeje@ymail.com>
2023-08-24 20:01:43 -0700

    Jeje commits

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,20 +1,19 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * free_list - frees a list_t list
- * @head: pointer to the head of the list
- *
- * Return: void
+ * free_list - frees a linked list
+ * @head: list_t list to be freed
  */
 void free_list(list_t *head)
 {
-	list_t *tmp;
+	list_t *temp;
 
 	while (head)
 	{
-		tmp = head;
-		head = head->next;
-		free(tmp->str);
-		free(tmp);
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
 	}
 }
