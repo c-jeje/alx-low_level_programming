c1fcab2c311defeb711201fe54c8092cbd229b14
c-jeje <charltonijeje@ymail.com>
2023-08-29 15:30:24 -0700

    jeje

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
-/** 
- *_strncat - appends the src string to the dest string
+/**
+ * _strncat - appends the src string to the dest string
  * @dest: variable pointed
  * @src: variable string pointed
  * @n: variable counter
@@ -12,7 +12,7 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int c_dest;
 	int c_src;
-	
+
 	c_dest = 0;
 	c_src = 0;
 
@@ -28,6 +28,6 @@ char *_strncat(char *dest, char *src, int n)
 	}
 
 	dest[c_dest] = '\0';
-	
+
 	return (dest);
 }
